c7a1701dfab38d2d5afe0167dfa47ea4f098016d
Alberto Marnetto <16267343+AlbertoMarnetto@users.noreply.github.com>
2025-02-01 10:42:46 +0100

    Add #define for debug infos

--- src/restunts/c/frame.c
+++ src/restunts/c/frame.c
@@ -3,6 +3,7 @@
 #include "shape3d.h"
 
 #define TILES_TO_DRAW_COUNT 110
+#define DISPLAY_DEBUG_INFO 0
 
 extern struct RECTANGLE* rectptr_unk2;
 extern struct RECTANGLE rect_array_unk[];
@@ -1279,12 +1280,14 @@ start_rendering:
 		}
 	}
 
+#if DISPLAY_DEBUG_INFO
 	// Debug: print discarded tiles
 	_sprintf(discarded_tiles_str, "Disc: %d", discarded_tiles);
 	font_set_fontdef2(fontnptr);
 	si = discarded_tiles == 0 ? 11 : discarded_tiles < 30 ? 14 : 12; // cyan, yellow, red
 	rect_union(intro_draw_text(discarded_tiles_str, 0x0C, roofbmpheight + 2, si, 0), &rect_unk11, &rect_unk11);
 	font_set_fontdef();
+#endif
 
 	if (timertestflag_copy != 0) {
 		rect_union(draw_ingame_text(), rect_unk, rect_unk);
