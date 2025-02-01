e2bfba635ad900a53aef0348c893ada9edc228e4
Alberto Marnetto <16267343+AlbertoMarnetto@users.noreply.github.com>
2025-02-01 10:39:41 +0100

    Optimize tiles

--- src/tiles_gen/coords.py
+++ src/tiles_gen/coords.py
@@ -2,29 +2,32 @@
 
 import math
 
+# Ensure this is the same as in frame.c
+TILES_TO_DRAW_COUNT = 110
+
 # Function to calculate the distance from the origin
 def distance_from_origin(x, y):
-    return math.sqrt(x**2 + 0.4 * y**2)
+    return math.sqrt(x**2 + 0.4 * (y + 0.5)**2)
 
 # Generate coordinate pairs and their distances
 coordinates = []
 for x in range(-20, 21):
     for y in range(-20, 21):
         # Apply the filter condition
-        if y >= abs(x) * 0.5 - 0.25:
+        if y >= abs(x) * 0.5 - 2:
             coordinates.append((x, y, distance_from_origin(x, y)))
 
 # Sort coordinates by their distance from the origin
 sorted_coordinates = sorted(coordinates, key=lambda coord: coord[2])
 
-# Extract the first 150 coordinate pairs
-first_150_coordinates = sorted_coordinates[:150]
+# Extract the first coordinate pairs
+first_coordinates = sorted_coordinates[:TILES_TO_DRAW_COUNT]
 
 ## Print the coordinate pairs
-#for index, (x, y, dist) in enumerate(first_150_coordinates, start=1):
+#for index, (x, y, dist) in enumerate(first_coordinates, start=1):
 #    print(f"{index}: ({x}, {y}) ({distance_from_origin(x, y)})")
 
 print('struct TILE_REL_COORDS lookahead_tiles[TILES_TO_DRAW_COUNT] = {')
-u = ',\n'.join([f'{{ {c[0]:3}, {c[1]:3} }}' for c in reversed(sorted_coordinates[:110])])
+u = ',\n'.join([f'{{ {c[0]:3}, {c[1]:3} }}' for c in reversed(first_coordinates)])
 print(u)
 print('};')
