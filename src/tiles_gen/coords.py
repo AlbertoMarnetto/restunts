66dc310c500af9864ef16dfd8c2c45902940abcc
Alberto Marnetto <16267343+AlbertoMarnetto@users.noreply.github.com>
2025-02-03 03:48:45 +0100

    Rework tiles list

--- src/tiles_gen/coords.py
+++ src/tiles_gen/coords.py
@@ -7,7 +7,7 @@ TILES_TO_DRAW_COUNT = 110
 
 # Function to calculate the distance from the origin
 def distance_from_origin(x, y):
-    return math.sqrt(x**2 + 0.4 * (y + 0.5)**2)
+    return math.sqrt(x**2 + 0.4 * (y + 0.49)**2)
 
 # Generate coordinate pairs and their distances
 coordinates = []
@@ -27,7 +27,7 @@ first_coordinates = sorted_coordinates[:TILES_TO_DRAW_COUNT]
 #for index, (x, y, dist) in enumerate(first_coordinates, start=1):
 #    print(f"{index}: ({x}, {y}) ({distance_from_origin(x, y)})")
 
-print('struct TILE_REL_COORDS lookahead_tiles[TILES_TO_DRAW_COUNT] = {')
+print('struct TILE_REL_COORDS lookahead_tiles_supersight[TILES_TO_DRAW_COUNT] = {')
 u = ',\n'.join([f'{{ {c[0]:3}, {c[1]:3} }}' for c in reversed(first_coordinates)])
 print(u)
 print('};')
