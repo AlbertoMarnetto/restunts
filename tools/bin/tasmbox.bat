86c8246765af991be09e74630132cfa01e5289c2
LowLevelMahn <dl.soluz@gmx.net>
2025-03-15 12:48:57 +0100

    replaces borland make with gnu make

--- tools/bin/tasmbox.bat
+++ tools/bin/tasmbox.bat
@@ -1,20 +1,29 @@
 @echo off
-del tasm.dosbox.conf 2>nul
-copy %~dp0dosbox.tmpl.conf tasm.dosbox.conf >nul
-echo [autoexec] >>tasm.dosbox.conf
-echo MOUNT S S:\ >>tasm.dosbox.conf
-echo S: >>tasm.dosbox.conf
-echo SET PATH=%%PATH%%;s:\tools\bin >>tasm.dosbox.conf
-echo cd %CD% >>tasm.dosbox.conf
-echo del %5 >>tasm.dosbox.conf
-echo tasmx %* ^>tasmbox.out >>tasm.dosbox.conf
-echo if ERRORLEVEL 0 goto noerror >>tasm.dosbox.conf
-echo type tasmbox.out >>tasm.dosbox.conf
-echo pause >>tasm.dosbox.conf
-echo :noerror >>tasm.dosbox.conf
-echo exit >>tasm.dosbox.conf
+
+set SRC_CFG=%~dp0dosbox.tmpl.conf
+set DST_CFG=%~dp0tasm.dosbox.conf
+
+del %DST_CFG% 2>nul
+copy %SRC_CFG% %DST_CFG% >nul
+
+(
+  echo [autoexec]
+  echo MOUNT S S:\
+  echo S:
+  echo SET PATH=s:\tools\bin;%%PATH%%
+  echo cd %CD%
+  echo del %5
+  echo tasmx %* ^>tasmbox.out
+  echo if ERRORLEVEL 0 goto noerror
+  echo type tasmbox.out
+  echo pause
+  echo :noerror
+  echo exit
+) >> %DST_CFG%
+
 set SDL_VIDEODRIVER=dummy
-dosbox -conf tasm.dosbox.conf -noconsole
-del tasm.dosbox.conf 2>nul
+dosbox -conf %DST_CFG% -noconsole
+
+del %DST_CFG% 2>nul
 type tasmbox.out
 del tasmbox.out 2>nul
